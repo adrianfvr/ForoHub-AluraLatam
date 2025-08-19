0f48e76f5e071b76ba74961b99646cef8b361468
Adrian Vargas <adrianfelipevr@gmail.com>
2025-08-19 12:29:29 -0500

    Completando Migracion

--- src/main/java/forohub/api/controller/TopicoController.java
+++ src/main/java/forohub/api/controller/TopicoController.java
@@ -4,16 +4,18 @@ import forohub.api.topico.DatosRegistroTopico;
 import forohub.api.topico.Topico;
 import forohub.api.topico.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/topicos")
+@RequestMapping("/topicos1")
 public class TopicoController {
     @Autowired
     private TopicoRepository repository;
+    @Transactional
     @PostMapping
     public void registrar(@RequestBody DatosRegistroTopico datos) {
         repository.save(new Topico(datos));
--- src/main/resources/db/migration/V1__create-table-topicos.sql
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -2,6 +2,10 @@ create table topicos(
     id bigint not null auto_increment,
     titulo varchar(150) not null,
     mensaje varchar(255) not null,
-    fecha_de_creacion
-    primary key(id);
+    fecha_de_creacion date not null,
+    status varchar(100) not null,
+    autor varchar(100) not null,
+    curso varchar(255) not null,
+
+    primary key(id)
 );
